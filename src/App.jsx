0714e60583e1be6ef04eec4b62f0984eec8f7e7e
liyanagedunanjana2010-glitch <liyanagedunanjana2010@gmail.com>
2026-08-07 14:10:17 +0530

    Remove missing About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import About from './components/About';
 import Services from './components/Services';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,6 @@ const Navbar = () => {
 
   const navLinks = [
     { name: 'Home', href: '#home' },
-    { name: 'About', href: '#about' },
     { name: 'Services', href: '#services' },
     { name: 'Portfolio', href: '#portfolio' },
     { name: 'Contact', href: '#contact' },
