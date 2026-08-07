27b4befbe873074473dd583a7adfe113f2b253ef
liyanagedunanjana2010-glitch <liyanagedunanjana2010@gmail.com>
2026-08-07 13:23:52 +0530

    Add floating WhatsApp button

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import Services from './components/Services';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
+import WhatsAppButton from './components/WhatsAppButton';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Services />
       <Portfolio />
       <Contact />
+      <WhatsAppButton />
     </div>
   );
 }
